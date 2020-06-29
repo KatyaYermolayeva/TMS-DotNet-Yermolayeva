3a5da7814ba9822b50c1c75a43175a2af59f7b00
KatyaYermolayeva <50108213+KatyaYermolayeva@users.noreply.github.com>
2020-06-29 17:38:27 +0300

    feat: add class Animal

---
+++ src/Homework-4/Models/Animal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Homework_4.Enums;
+
+namespace Homework_4.Models
+{
+    class Animal
+    {
+        private readonly Guid _id = Guid.NewGuid();
+        private string _passport;
+
+        public Animal()
+        {
+            Name = "No name";
+            Kind = KindType.None;
+            _passport = Guid.NewGuid().ToString();
+        }
+
+        public Animal(string name)
+        {
+            Name = name;
+            Kind = KindType.None;
+            _passport = Guid.NewGuid().ToString();
+        }
+
+        public Animal(string name, KindType kind)
+        {
+            Name = name;
+            Kind = kind;
+            _passport = Guid.NewGuid().ToString();
+        }
+
+        public Animal(string passport, string name, KindType kind)
+        {
+            Name = name;
+            Kind = kind;
+            _passport = passport;
+        }
+
+        public string Name { get; set; }
+        
+        public KindType Kind { get; set; }
+       
+        public string GetPassport()
+        {
+            return _passport;
+        }
+       
+        public Guid GetId()
+        {
+            return _id;
+        }
+        
+        public void SetPassport(string passport)
+        {
+            _passport = passport;
+        }
+    }
+}
