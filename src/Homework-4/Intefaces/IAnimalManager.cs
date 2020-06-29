1c076cc621113492295fe37479960ade0ca642ae
KatyaYermolayeva <50108213+KatyaYermolayeva@users.noreply.github.com>
2020-06-29 17:42:58 +0300

    feat: add interface IAnimalManager

---
+++ src/Homework-4/Intefaces/IAnimalManager.cs
@@ -0,0 +1,11 @@
+﻿using Homework_4.Models;
+
+namespace Homework_4.Intefaces
+{
+    interface IAnimalManager
+    {
+        public void Rename(Animal animal, string name);
+        public void GetInfo(Animal animal);
+        void SetPassport(Animal animal, string passport);
+    }
+}
