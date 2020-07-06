32122e7dc9a0cc35f211aaf38a4bc7c721cc1756
KatyaYermolayeva <50108213+KatyaYermolayeva@users.noreply.github.com>
2020-07-06 17:34:20 +0300

    feat: add class Result, add class Person

---
+++ src/FitnessCore/Models/Result.cs
@@ -0,0 +1,16 @@
+﻿using FitnessCore.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessCore.Models
+{
+    public class Result
+    {
+        public DateTime Date { get; set; }
+
+        public ExerciseType Type { get; set; }
+
+        public double Calories { get; set; }
+    }
+}
