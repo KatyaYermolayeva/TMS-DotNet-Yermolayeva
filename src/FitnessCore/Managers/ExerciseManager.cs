2d91bcf7bf29a4eb27d548e5c09ab0d45cbf4299
KatyaYermolayeva <50108213+KatyaYermolayeva@users.noreply.github.com>
2020-07-06 17:37:54 +0300

    feat: add class ExerciseManager

---
+++ src/FitnessCore/Managers/ExerciseManager.cs
@@ -0,0 +1,34 @@
+﻿using FitnessCore.Enums;
+using FitnessCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessCore.Managers
+{
+    class ExerciseManager : IExerciseManager
+    {
+        private Dictionary<ExerciseType, double> CaloriesPerHourPerKilogram = new Dictionary<ExerciseType, double>()
+        {
+            { ExerciseType.Runing, 6.4},
+            { ExerciseType.Skating, 3.2},
+            { ExerciseType.Aerobics, 5.2},
+            { ExerciseType.Cycing, 5.4},
+            { ExerciseType.Gymnastics, 4},
+            { ExerciseType.Streching, 1.8},
+            { ExerciseType.Swiming, 6}
+        };
+        public double Exercise(ExerciseType type, double hours, double weight)
+        {
+            if (hours <= 0)
+            {
+                throw new ArgumentException("Invalid input: hours < 0.");
+            }
+            if (weight <= 0)
+            {
+                throw new ArgumentException("Invalid input: weight < 0.");
+            }
+            return CaloriesPerHourPerKilogram.GetValueOrDefault(type) * hours * weight;
+        }
+    }
+}
