57d767ff2b58a2f3a857b84d652545b67b3f89fe
KatyaYermolayeva <50108213+KatyaYermolayeva@users.noreply.github.com>
2020-07-06 17:35:24 +0300

    feat: add interface IExerciseManager

---
+++ src/FitnessCore/Interfaces/IExerciseManager.cs
@@ -0,0 +1,12 @@
+﻿using FitnessCore.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessCore.Interfaces
+{
+    public interface IExerciseManager
+    {
+        double Exercise(ExerciseType type, double minutes, double weight);
+    }
+}
