f9f40fa4a16319f0d28232123c2cc646b847aac3
KatyaYermolayeva <50108213+KatyaYermolayeva@users.noreply.github.com>
2020-07-16 16:21:58 +0300

    feat: add class Customer

---
+++ src/Homework-7/Customer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework_7
+{
+    class Customer
+    {
+        private static int _globalId = 0;
+        private readonly int _id;
+        public int ShoppingSpeed { get; set; }
+        public IList<Product> Basket { get; set; } = new List<Product>();
+
+        public Customer()
+        {
+            _id = ++_globalId;
+            FillBasket();
+            Name = $"Customer {_id}";
+            var rand = new Random();
+            ShoppingSpeed = rand.Next(100, 1000);
+        }
+
+        public Customer(int basketSize)
+        {
+            _id = ++_globalId;
+            Name = $"Customer {_id}";
+            FillBasket(basketSize);
+            var rand = new Random();
+            ShoppingSpeed = rand.Next(100, 1000);
+        }
+
+        public Customer(int basketSize, int shoppingSpeed)
+        {
+            _id = ++_globalId;
+            Name = $"Customer {_id}";
+            FillBasket(basketSize);
+            ShoppingSpeed = shoppingSpeed;
+        }
+        public void FillBasket()
+        {
+            var random = new Random();
+            int productsCount = random.Next(1, 15);
+            for (int i = 0; i < productsCount; i++)
+            {
+                Basket.Add(new Product());
+            }
+        }
+
+        public void FillBasket(int productsCount)
+        {
+            if (productsCount < 1)
+            {
+                throw new ArgumentException("Invalid argument - productsCount < 1.");
+            }
+            for (int i = 0; i < productsCount; i++)
+            {
+                Basket.Add(new Product());
+            }
+        }
+
+        public string Name { get; set; }
+        public int BasketSize
+        {
+            get
+            {
+                return Basket.Count;
+            }
+        }
+    }
+}
