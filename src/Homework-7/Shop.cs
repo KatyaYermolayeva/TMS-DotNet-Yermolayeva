fc56547b8cc5171ca971aca52711794e329318ea
KatyaYermolayeva <50108213+KatyaYermolayeva@users.noreply.github.com>
2020-07-16 16:23:40 +0300

    feat: add class Shop

---
+++ src/Homework-7/Shop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Homework_7
+{
+    class Shop
+    {
+        public List<Cashier> Cashiers;
+        public bool IsOpen;
+        public Shop()
+        {
+            Name = "The Shop";
+            Cashiers = new List<Cashier>();
+        }
+
+        public Shop(int cashiersCount)
+        {
+            Name = "The Shop";
+            Cashiers = new List<Cashier>();
+            for (int i = 0; i < cashiersCount; i++)
+            {
+                Cashiers.Add(new Cashier());
+            }
+        }
+
+        public string Name { get; set; }
+    }
+}
