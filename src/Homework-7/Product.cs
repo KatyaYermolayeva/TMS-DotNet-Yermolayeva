36f835def35661e898b08dd3b693d68896b458a4
KatyaYermolayeva <50108213+KatyaYermolayeva@users.noreply.github.com>
2020-07-16 16:21:06 +0300

    feat: add class Product

---
+++ src/Homework-7/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework_7
+{
+    class Product
+    {
+        public string Code { get; set; } = Guid.NewGuid().ToString();
+        public Decimal Price { get; set; }
+
+        public Product()
+        {
+            var random = new Random();
+            Price = random.Next(10, 1000);
+        }
+
+        public override string ToString()
+        {
+            return Code;
+        }
+    }
+}
